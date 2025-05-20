3391338f30d3ac478a2e63b0acafe7d516f1517a
Sander Halvorsen <sandhal@stud.ntnu.no>
2025-05-20 23:39:10 +0200

    trying to get connection right again :(

--- setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-
-setup(
-    name="twitter_clone",
-    version="0.1",
-    packages=find_packages(),       # finds your 'app' package
-    include_package_data=True,
-)
